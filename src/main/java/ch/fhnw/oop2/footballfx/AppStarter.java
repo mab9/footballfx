674db06a79be51572469840769446f93c90ee5b6
Marc-Antoine Bruelhart <marc-antoine.bruelhart@bedag.ch>
2017-05-02 19:51:14 +0200

    add resources to gitignore

--- src/main/java/ch/fhnw/oop2/footballfx/AppStarter.java
+++ src/main/java/ch/fhnw/oop2/footballfx/AppStarter.java
@@ -12,9 +12,7 @@ public class AppStarter extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         PresentationModel pm = new PresentationModel();
-
         Parent rootPanel = new ApplicationUI(pm);
-
         Scene scene = new Scene(rootPanel);
 
         primaryStage.titleProperty().bind(pm.applicationTitleProperty());
