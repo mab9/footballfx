5547ea1caba8e85a40a2c11249120bf7b7e2df9f
peer.juettner <alleaussis@gmail.com>
2017-05-06 14:28:02 +0200

    Added Layoutelements (all except toolbar) and added basic styling.

--- src/main/java/ch/fhnw/oop2/footballfx/view/ApplicationUI.java
+++ src/main/java/ch/fhnw/oop2/footballfx/view/ApplicationUI.java
@@ -3,9 +3,11 @@ package ch.fhnw.oop2.footballfx.view;
 import ch.fhnw.oop2.footballfx.presentationmodel.PresentationModel;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class ApplicationUI extends StackPane {
+public class ApplicationUI extends HBox {
 
     private final PresentationModel model;
 
@@ -19,14 +21,30 @@ public class ApplicationUI extends StackPane {
     private ImageView countryImageView; // IV für Landesflagge
     private ImageView teamImageView; // IV für Teamlogo
     private ToolBar toolBar; // Toolbar für Buttons
-    private Label playerName; // Label für name
-    private Label playerCountry; // Label für Country
-    private Label playerLaenderspiele; // Label für Anzahl länderspiele (Nummer)
+    private TextField playerName; // Label für name
+    private TextField playerCountry; // Label für Country
+    private TextField playerLaenderspiele; // Label für Anzahl länderspiele (Nummer)
     private Label textLaenderspiele; // Label für text "Länderspiele"
-    private Label playerStartYear; // Label für startjahr
-    private Label playerEndYear; // Label für endjahr
+    private TextField playerStartJahr; // Label für startjahr
+    private TextField playerEndJahr; // Label für endjahr
+    private TextField playerGegen; // Label für endjahr
+    private TextField playerFifa; // Label für endjahr
+    private TextField playerRSSSF; // Label für endjahr
+    private TextField playerPosition; // Label für endjahr
+    private TextField playerGeboren; // Label für endjahr
+    private TextField playerHundertesSpiel; // Label für endjahr
+    private TextField playerErstesSpiel; // Label für endjahr
+    private TextField playerLetztesSpiel; // Label für endjahr
+    private TextField playerVerband; // Label für endjahr
+    private Label playerRang; // Label für endjahr
     private Label textName; // Label für text "NAme"
+    private Label overName; // Label für text "NAme"
+    private Label overLand; // Label für text "NAme"
+    private Label overLaenderspiele; // Label für text "NAme"
+    private Label overStartJahr; // Label für text "NAme"
+    private Label overEndJahr; // Label für text "NAme"
     private Label textLand; // Label für text "Land"
+    private Label textBis; // Label für text "Bis"
     private Label textVerband; // Label für text "Verband"
     private Label textSpieleFifa; // Label für text "Spiele Fifa"
     private Label textSpieleRSSSF; // Label für text "Spiele RSSSF"
@@ -56,6 +74,14 @@ public class ApplicationUI extends StackPane {
     }
 
     private void initializeControls() {
+
+        /* Controls
+        text* = Labels, welche Statischen text darstellen, wie z.B "Name","Verband" etc.
+        over* = Labels, welche in der Overview verwendet werden und dynamisch nach Auswahl sich anpassen
+        player* = TextFields, welche zur Eingabe im Editorbereich verwendet werden. Ausnahme: PlayerRand = Label, weil nicht vom User veränderbar,
+
+
+         */
         // Init buttons
         buttonAdd = new Button();
         buttonRemove = new Button();
@@ -77,11 +103,23 @@ public class ApplicationUI extends StackPane {
         toolBar = new ToolBar();
 
         // init Labels
-        playerName = new Label();
-        playerCountry = new Label();
-        playerLaenderspiele = new Label();
-        playerStartYear = new Label();
-        playerEndYear = new Label();
+        playerName = new TextField();
+        playerCountry = new TextField();
+        playerLaenderspiele = new TextField();
+        playerStartJahr = new TextField();
+        playerEndJahr = new TextField();
+        playerVerband = new TextField();
+        playerFifa = new TextField();
+        playerRSSSF = new TextField();
+        playerHundertesSpiel = new TextField();
+        playerErstesSpiel = new TextField();
+        playerRang = new Label();
+        playerGeboren = new TextField();
+        playerPosition = new TextField();
+        playerRang= new Label();
+        playerRSSSF= new TextField();
+        playerGegen= new TextField();
+        playerLetztesSpiel= new TextField();
         textLaenderspiele = new Label();
         textName= new Label();
         textLand= new Label();
@@ -96,12 +134,99 @@ public class ApplicationUI extends StackPane {
         textGegen= new Label();
         textErstesSpiel= new Label();
         textLetztesSpiel= new Label();
+        textBis = new Label();
+        overName = new Label();
+        overLand = new Label();
+        overEndJahr = new Label();
+        overStartJahr = new Label();
+        overLaenderspiele = new Label();
+
+
+
+        // Set Layout Gaps
+
 
 
     }
 
     private void layoutControls() {
-        getChildren().add(button);
+        GridPane gridTop = new GridPane();
+        gridTop.getStyleClass().add("gridTop");
+        GridPane gridBottom = new GridPane();
+        gridBottom.getStyleClass().add("gridBottom");
+        VBox vBoxright = new VBox();
+        vBoxright.getChildren().addAll(gridTop,gridBottom);
+        vBoxright.setSpacing(10);
+        gridTop.setHgap(3);
+        gridTop.setVgap(3);
+
+
+
+        gridTop.add(overName,0,0,4,1);
+        gridTop.add(overLand,0,1);
+        gridTop.add(countryImageView,3,1,1,2);
+        gridTop.add(overLaenderspiele,0,2);
+        gridTop.add(textLaenderspiele,1,2);
+        gridTop.add(teamImageView,3,2,1,2);
+        gridTop.add(overStartJahr,0,3);
+        gridTop.add(textBis,1,3);
+        gridTop.add(overEndJahr,2,3);
+
+        // Column 0
+        gridBottom.add(textName,0,0);
+        gridBottom.add(textLand,0,1);
+        gridBottom.add(textVerband,0,2);
+        gridBottom.add(textSpieleFifa,0,3);
+        gridBottom.add(textHundertesSpiel,0,4);
+        gridBottom.add(textErstesSpiel,0,5);
+        // Column 1
+        gridBottom.add(playerName,1,0);
+        gridBottom.add(playerCountry,1,1);
+        gridBottom.add(playerVerband,1,2);
+        gridBottom.add(playerFifa,1,3);
+        gridBottom.add(playerHundertesSpiel,1,4);
+        gridBottom.add(playerErstesSpiel,1,5);
+
+        // Column 2
+        gridBottom.add(textGeboren,2,0);
+        gridBottom.add(textPosition,2,1);
+        gridBottom.add(textRang,2,2);
+        gridBottom.add(textSpieleRSSSF,2,3);
+        gridBottom.add(textGegen,2,4);
+        gridBottom.add(textLetztesSpiel,2,5);
+
+        //Column 3
+        gridBottom.add(playerGeboren,3,0);
+        gridBottom.add(playerPosition,3,1);
+        gridBottom.add(playerRang,3,2);
+        gridBottom.add(playerRSSSF,3,3);
+        gridBottom.add(playerGegen,3,4);
+        gridBottom.add(playerLetztesSpiel,3,5);
+
+        // Text for Overview
+        overName.setText("Ahmed Hassan");
+        overLand.setText("Ägypten");
+        overLaenderspiele.setText("184");
+        textLaenderspiele.setText("Länderspiele");
+        overStartJahr.setText("2020");
+        textBis.setText("bis");
+        overEndJahr.setText("2021");
+
+        //Text for Editor
+        textName.setText("Name");
+        textLand.setText("Land");
+        textVerband.setText("Verband");
+        textSpieleFifa.setText("Spiele (FIFA)");
+        textSpieleRSSSF.setText("Spiele (RSSSF)");
+        textHundertesSpiel.setText("100. Spiel");
+        textGegen.setText("gegen");
+        textErstesSpiel.setText("erstes Spiel");
+        textLetztesSpiel.setText("letztes Spiel");
+        textGeboren.setText("geboren");
+        textPosition.setText("position");
+        textRang.setText("Rang");
+
+        getChildren().addAll(playerTableView,vBoxright);
     }
 
     private void setupEventHandlers() {
@@ -111,6 +236,6 @@ public class ApplicationUI extends StackPane {
     }
 
     private void setupBindings() {
-        button.textProperty().bind(model.greetingProperty());
+        //button.textProperty().bind(model.greetingProperty());
     }
 }
