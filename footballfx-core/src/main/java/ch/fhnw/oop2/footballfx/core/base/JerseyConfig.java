fdc7a58d93beaf475335afb6249edac7a6ca70ba
Marc-Antoine Bruelhart <marc-antoine.bruelhart@bedag.ch>
2017-05-28 18:47:11 +0200

    add jersey config and register endpoint

---
+++ footballfx-core/src/main/java/ch/fhnw/oop2/footballfx/core/base/JerseyConfig.java
@@ -0,0 +1,14 @@
+package ch.fhnw.oop2.footballfx.core.base;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+import ch.fhnw.oop2.footballfx.core.player.api.PlayerApi;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        register(PlayerApi.class);
+    }
+}
--- footballfx-core/src/main/java/ch/fhnw/oop2/footballfx/core/player/api/PlayerApi.java
+++ footballfx-core/src/main/java/ch/fhnw/oop2/footballfx/core/player/api/PlayerApi.java
@@ -13,7 +13,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
@@ -33,7 +35,9 @@ public class PlayerApi {
         this.playerService = playerService;
     }
 
+
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Player> retrieveAllPlayers() {
         return playerService.retrieveAllPlayers();
     }
