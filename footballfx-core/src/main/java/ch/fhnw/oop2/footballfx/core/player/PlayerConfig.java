dd37eb6400b303e6c718cb98df6698bbb633f733
Marc-Antoine Bruelhart <marc-antoine.bruelhart@bedag.ch>
2017-06-01 09:00:35 +0200

    - add spring bean injection helper for jersey

--- footballfx-core/src/main/java/ch/fhnw/oop2/footballfx/core/player/PlayerConfig.java
+++ footballfx-core/src/main/java/ch/fhnw/oop2/footballfx/core/player/PlayerConfig.java
@@ -3,7 +3,6 @@ package ch.fhnw.oop2.footballfx.core.player;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import ch.fhnw.oop2.footballfx.core.player.api.PlayerApi;
 import ch.fhnw.oop2.footballfx.core.player.business.PlayerService;
 import ch.fhnw.oop2.footballfx.core.player.dataaccess.PlayerRepository;
 
@@ -11,11 +10,6 @@ import ch.fhnw.oop2.footballfx.core.player.dataaccess.PlayerRepository;
 public class PlayerConfig {
 
     public PlayerConfig() {
-    }
-
-    @Bean
-    public PlayerApi playerApi() {
-        return new PlayerApi(playerService());
     }
 
     @Bean
